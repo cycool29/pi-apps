bcebb8aa6df36462b4b46ae4ed421e4db8701f52
cycool29 <cycool29@gmail.com>
2022-02-06 14:15:42 +0800

    Update Arduino.sh

--- .github/workflows/updates/Arduino.sh
+++ .github/workflows/updates/Arduino.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# webVer="$(wget -qO- https://www.arduino.cc/en/software | grep md5sum.txt | sed s/.md5sum.txt.*//g | sed 's+.*href="//downloads.arduino.cc/arduino-++g')"
+webVer="$(wget -qO- https://www.arduino.cc/en/software | grep md5sum.txt | sed s/.md5sum.txt.*//g | sed 's+.*href="//downloads.arduino.cc/arduino-++g')"
 armhf_url="https://downloads.arduino.cc/arduino-${webVer}-linuxarm.tar.xz"
 arm64_url="https://downloads.arduino.cc/arduino-${webVer}-linuxaarch64.tar.xz"
 
